f97e87f73be2bb5c6a9e64d32bd43de607cc9da1
Masiur Rahman Siddiki <masiur.sust@csesociety.org>
2017-02-25 14:45:57 +0600

    Api Updated function for show score

--- app/Http/Controllers/ScoreController.php
+++ app/Http/Controllers/ScoreController.php
@@ -22,21 +22,27 @@ class ScoreController extends Controller
      */
     public function show(Request $request)
     {
-        return $data = $request->all();
-        // try{
-        //     $score = Score::where('url', $linkV)->first();
-            
-        //     if($score != null){
-        //             $finalScore = $score->score_one + $score->score_two + $score->score_three + $score->score_four + $score->score_five;
-        //             $finalScore = $finalScore/5;
-        //         return Response::json(['message' => 'Success','score' => $finalScore], 200);
-        //     }else{
-        //         return Response::json(['message' => 'No data Found','score' => 0 ], 200);
-        //     }
+        $data = $request->all();
+        $receivedLink = $data['link'];
 
-        // }catch (Exception $e){
-        //     return Response::json(['message' => 'Something went wrong', 'error_code' => 403], 403);
-        // }
+        try{
+            $score = Score::where('url', $receivedLink)->first();
+            
+            if($score != null){
+
+                $finalScore = $score->score_one + $score->score_two + $score->score_three + $score->score_four + $score->score_five;
+                    $finalScore = $finalScore/5;
+                return Response::json(['message' => 'Success','score' => $finalScore], 200);
+            }else{
+                $ss = new Score();
+                $finalScore = $score->score_one + $score->score_two + $score->score_three + $score->score_four + $score->score_five;
+                $finalScore = $finalScore/5;
+                return Response::json(['message' => 'Success','score' => $finalScore], 200);
+            }
+
+        }catch (Exception $e){
+            return Response::json(['message' => 'Something went wrong', 'error_code' => 403], 403);
+        }
     }
 
 
@@ -55,54 +61,54 @@ class ScoreController extends Controller
         return  $request->all();
 
 
-        // try{
-        //     $score = Score::where('url','LIKE', $request->link)->first();
-        //     if($score == null) {
-        //             $ss = new Score();
-        //             $ss->url = $request->link;
-        //             if($request->rating != null  && $request->rating ==1){
-        //                 $ss->score_one = 1;
-        //             }elseif($request->rating != null && $request->rating ==2){
+        try{
+            $score = Score::where('url','LIKE', $request->link)->first();
+            if($score == null) {
+                    $ss = new Score();
+                    $ss->url = $request->link;
+                    if($request->rating != null  && $request->rating ==1){
+                        $ss->score_one = 1;
+                    }elseif($request->rating != null && $request->rating ==2){
 
-        //                 $ss->score_two = 2;
-        //             }elseif($request->rating != null && $request->rating ==3){
-        //                 $ss->score_three = 3;
-        //             }elseif($request->rating != null && $request->rating ==4){
-        //                 $ss->score_four =4;
-        //             }elseif($request->rating != null && $request->rating ==5){
+                        $ss->score_two = 2;
+                    }elseif($request->rating != null && $request->rating ==3){
+                        $ss->score_three = 3;
+                    }elseif($request->rating != null && $request->rating ==4){
+                        $ss->score_four =4;
+                    }elseif($request->rating != null && $request->rating ==5){
 
-        //                 $ss->score_five =5;
-        //             }
+                        $ss->score_five =5;
+                    }
 
-        //             $ss->save();
+                    $ss->save();
 
-        //         return Response::json(['success' => 'Posted Successfully'], 200);
+                return Response::json(['success' => 'Posted Successfully'], 200);
 
-        //     }else{
-        //         $ss =Score::where('url', 'LIKE', $request->link)->first();
-        //         $ss->url = $request->link;
-        //         if($request->rating != null && $request->rating ==1){
-        //             $ss->score_one =$score->score_one+ 1;
-        //         }elseif($request->rating != null && $request->rating ==2){
-        //             $ss->score_two = $score->score_two+ 2;
-        //         }elseif($request->rating != null && $request->rating ==3){
+            }else{
+                $ss =Score::where('url', 'LIKE', $request->link)->first();
+                $ss->url = $request->link;
+                if($request->rating != null && $request->rating ==1){
+                    $ss->score_one =$score->score_one+ 1;
+                }elseif($request->rating != null && $request->rating ==2){
+                    $ss->score_two = $score->score_two+ 2;
+                }elseif($request->rating != null && $request->rating ==3){
 
-        //             $ss->score_three = $score->score_three+ 3;
-        //         }elseif($request->rating != null && $request->rating ==4){
+                    $ss->score_three = $score->score_three+ 3;
+                }elseif($request->rating != null && $request->rating ==4){
 
-        //             $ss->score_four = $score->score_four+ 4;
-        //         }elseif($request->rating != null && $request->rating ==5){
+                    $ss->score_four = $score->score_four+ 4;
+                }elseif($request->rating != null && $request->rating ==5){
 
-        //             $ss->score_five =$score->score_five+ 5;
-        //         }
+                    $ss->score_five =$score->score_five+ 5;
+                }
 
-        //         $ss->save();
-        //         return Response::json(['success' => 'Posted Successfully'], 200);
-        //     }
+                $ss->save();
+                return Response::json(['success' => 'Posted Successfully'], 200);
+            }
 
-        // }catch (Exception $e){
-        //     return Response::json(['message' => 'Something went wrong', 'error_code' => 403], 403);
-        // }
+        }catch (Exception $e){
+            return Response::json(['message' => 'Something went wrong', 'error_code' => 403], 403);
+        }
     }
 
 
